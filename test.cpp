059d40ad0b84ca07a7722e0faaa3c8f24b74d251
meh <meh@schizofreni.co>
2013-03-17 03:52:58 +0100

    test: use tabs everywhere

--- test.cpp
+++ test.cpp
@@ -5,12 +5,12 @@ class foo
 
 class bar
 {
-  public:
-    std::string
-    inspect (void)
-    {
-      return "#<bar: dabbah>";
-    }
+	public:
+		std::string
+		inspect (void)
+		{
+			return "#<bar: dabbah>";
+		}
 
 		std::string
 		to_string (void)
@@ -34,39 +34,39 @@ union omg
 
 enum class derp
 {
-  lol, wut, omg
+	lol, wut, omg
 };
 
 std::ostream& operator << (std::ostream& on, derp v)
 {
-  switch (v) {
-    case derp::lol:
-      on << "lol";
-      break;
+	switch (v) {
+		case derp::lol:
+			on << "lol";
+			break;
 
-    case derp::wut:
-      on << "wut";
-      break;
+		case derp::wut:
+			on << "wut";
+			break;
 
-    case derp::omg:
-      on << "omg";
-      break;
-  }
+		case derp::omg:
+			on << "omg";
+			break;
+	}
 
-  return on;
+	return on;
 }
 
 int
 main (int argc, char* argv[])
 {
-  std::string str = "34";
-  std::cout << inspect::value(str) << std::endl;
+	std::string str = "34";
+	std::cout << inspect::value(str) << std::endl;
 
-  foo a;
-  std::cout << inspect::value(a) << std::endl;
+	foo a;
+	std::cout << inspect::value(a) << std::endl;
 
-  bar b;
-  std::cout << inspect::value(b) << std::endl;
+	bar b;
+	std::cout << inspect::value(b) << std::endl;
 
 	wat c;
 	std::cout << inspect::value(c) << std::endl;
@@ -74,19 +74,19 @@ main (int argc, char* argv[])
 	omg d;
 	std::cout << inspect::value(d) << std::endl;
 
-  std::cout << inspect::value(23) << std::endl;
-  std::cout << inspect::value(derp::omg) << std::endl;
+	std::cout << inspect::value(23) << std::endl;
+	std::cout << inspect::value(derp::omg) << std::endl;
 
-  int lol[][2] = { { 1, 2 }, { 3, 4 } };
-  std::cout << inspect::value(lol) << std::endl;
+	int lol[][2] = { { 1, 2 }, { 3, 4 } };
+	std::cout << inspect::value(lol) << std::endl;
 
-  float* duh = (float*) 342;
-  std::cout << inspect::value(duh) << std::endl;
+	float* duh = (float*) 342;
+	std::cout << inspect::value(duh) << std::endl;
 
-  std::cout << inspect::value("lol") << std::endl;
+	std::cout << inspect::value("lol") << std::endl;
 
-  std::cout << inspect::value(true) << std::endl;
+	std::cout << inspect::value(true) << std::endl;
 
-  return 0;
+	return 0;
 }
 
